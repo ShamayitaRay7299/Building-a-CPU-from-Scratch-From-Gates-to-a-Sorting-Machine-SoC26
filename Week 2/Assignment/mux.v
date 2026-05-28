b9f9ff5157661101ccff3f2b9b53441043eedc76
ShamayitaRay7299 <shamayitaray2008@gmail.com>
2026-05-28 23:53:27 +0530

    Update mux.v

--- Week 2/Assignment/mux.v
+++ Week 2/Assignment/mux.v
@@ -23,4 +23,10 @@ module mux4 #(parameter WIDTH = 8) (
     output [WIDTH-1:0] y
 );
     // YOUR CODE HERE
+    assign y = 
+        (sel == 2'b00) ? d0:
+        (sel == 2'b01) ? d1:
+        (sel == 2'b10) ? d2:
+        d3;
+    
 endmodule
